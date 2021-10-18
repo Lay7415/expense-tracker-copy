5f22147dffda0414321b6dc683fd93ae7ca244b5
Azamat Orozbaev <sadchill4@gmail.com>
2021-10-18 16:56:35 +0600

    adding conditional rendering

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,29 +1,23 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2021');
 
     const filterChangeHundler = (selectedYear) => {
-        console.log(selectedYear);
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
     return <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHundler} />
-        {
-            props.expenses.map((el) => {
-                return <ExpenseItem
-                    title={el.title}
-                    amount={el.amount}
-                    date={el.date}
-                    key={el.id}
-                />
-            })
-        }
+        <ExpensesList expenses={filteredExpenses}/>
     </Card>
 }
 
