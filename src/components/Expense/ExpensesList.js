a8c19510d654d842a469aab60e27274c4e444622
Azamat Orozbaev <sadchill4@gmail.com>
2021-10-18 17:41:51 +0600

    finish

--- src/components/Expense/ExpensesList.js
+++ src/components/Expense/ExpensesList.js
@@ -3,12 +3,12 @@ import ExpenseItem from "./ExpenseItem";
 import './ExpensesList.css';
 
 const ExpensesList = props => {
-    if (props.expenses.length === 0) {
+    if (props.items.length === 0) {
         return <h2 className='expenses-list__fallback'>No Expense Found</h2>
     }
 
     return <ul className='expenses-list'>
-        {props.expenses.map((el) => {
+        {props.items.map((el) => {
             return <ExpenseItem
                 title={el.title}
                 amount={el.amount}
